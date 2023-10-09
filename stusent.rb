b54f7cca880ed48ff8ada13b0cd2eb545965e8d9
mumbereh <siirasonh@gmail.com>
2023-10-09 18:12:05 +0300

    configured linter as one of the project's requirement

--- stusent.rb
+++ stusent.rb
@@ -1,12 +1,12 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(id, classroom, name = "Unknown", age = 0, parent_permission = true)
-    super(id, name, age, parent_permission)
+  def initialize(id, classroom, name: 'Unknown', age: 0, parent_permission: true)
+    super(id, name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    "¯\\(ツ)/¯"
+    '¯\\(ツ)/¯'
   end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(id, specialization, name = "Unknown", age = 0, parent_permission = true)
-    super(id, name, age, parent_permission)
+  def initialize(id, specialization, name: 'Unknown', age: 0, parent_permission: true)
+    super(id, name: name, age: age, parent_permission: parent_permission)
     @specialization = specialization
   end
 
