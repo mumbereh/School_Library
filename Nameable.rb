4f3adf0e6e2a9655e8c449e61b5abb80c0a35118
mumbereh <siirasonh@gmail.com>
2023-10-10 20:52:41 +0300

    configured linters

--- Nameable.rb
+++ Nameable.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Nameable
   def correct_name
     raise NotImplementedError, "#{self.class} must implement the correct_name method"
--- person.rb
+++ person.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
