35c61c0b4cb620a98e31f27e001d58dcf103be1e
mumbere Habert <107393299+mumbereh@users.noreply.github.com>
2023-10-10 22:35:12 +0300

    Update Nameable.rb

--- Nameable.rb
+++ Nameable.rb
@@ -1,9 +1,4 @@
-<<<<<<< HEAD:name_able.rb
-# frozen_string_literal: true
 
-=======
-# Nameable interface
->>>>>>> parent of f323991 (configured linters):Nameable.rb
 class Nameable
   def correct_name
     raise NotImplementedError, 'Subclasses must implement correct_name method'
