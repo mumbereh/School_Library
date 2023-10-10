9f2c5d525e537a5d637c162848e32be450048cdb
mumbere Habert <107393299+mumbereh@users.noreply.github.com>
2023-10-10 22:35:53 +0300

    Update capitalize_decorator..rb

--- capitalize_decorator..rb
+++ capitalize_decorator..rb
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-# frozen_string_literal: true
-
-=======
-# CapitalizeDecorator
->>>>>>> parent of f323991 (configured linters)
 class CapitalizeDecorator < Decorator
   def correct_name
     super.capitalize
