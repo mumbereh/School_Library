959063636b819755c9dd226f4aa2cd3211a0d377
mumbere Habert <107393299+mumbereh@users.noreply.github.com>
2023-10-10 22:36:26 +0300

    Update trimmer_decorator.rb

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-# frozen_string_literal: true
-
-=======
-# TrimmerDecorator
->>>>>>> parent of f323991 (configured linters)
 class TrimmerDecorator < Decorator
   def correct_name
     super[0..9]
