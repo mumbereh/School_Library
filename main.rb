47f4bfa351b217f13e120184854a46b438255e46
mumbereh <siirasonh@gmail.com>
2023-10-11 07:16:48 +0300

    fix all changes required

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 require_relative 'nameable'
 require_relative 'decorator'
-require_relative 'capitalize_decorator' 
+require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
 
 
